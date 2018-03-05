330ded3a91b6042d85c3841ce04f974a00bf0dd1
davesag <davesag@gmail.com>
2018-03-05 11:28:39 +0530

    n/a fixed code coverage

---
+++ test/unit/api/v1/index_spec.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+
+describe('api/v1', () => {
+  const mockTraverse = sinon.spy()
+  const mockTraversal = () => mockTraverse
+  const index = proxyquire('../../../../src/api/v1', {
+    '../../utils/traversal': mockTraversal
+  })
+
+  it('called traversal', () => {
+    expect(mockTraverse).to.have.been.called
+  })
+})
